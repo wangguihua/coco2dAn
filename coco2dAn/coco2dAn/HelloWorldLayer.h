99086752d426d30c3fd1dcacd6d42c6a8a378d2e
wangguihua <329750074@qq.com>
2014-03-01 16:19:17 +0800

    coco2d learn2

---
+++ coco2dAn/coco2dAn/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  coco2dAn
+//
+//  Created by llbt_wgh on 14-3-1.
+//  Copyright llbt 2014年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
