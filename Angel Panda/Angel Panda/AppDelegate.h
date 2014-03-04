1550c1bcf44ee2f3f977c411182cc9d7923728f9
wangguihua <329750074@qq.com>
2014-03-04 17:43:25 +0800

    inint

---
+++ Angel Panda/Angel Panda/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  Angel Panda
+//
+//  Created by llbt_wgh on 14-3-2.
+//  Copyright llbt 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ Angel Panda/Angel Panda/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  Angel Panda
+//
+//  Created by llbt_wgh on 14-3-2.
+//  Copyright llbt 2014年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
